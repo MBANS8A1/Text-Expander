bf9ef565356554f017717fcc301a8eb58002093d
MBANS8A1 <seanmbng@aol.com>
2025-11-04 11:06:57 +0000

    I made a correction on line 49, where I unnecessarily added a limit argument to the split() function. Also, as the TextExpander component should be reusable it should not depend on any external CSS, so I made a custom buttonStyle object, which has been applied to the style of the button. The TextExpander by default has a blue buttonColor but it is overriden by the second TextExpander.

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function TextExpander({
   collapsedNumWords = 10,
   expandButtonText = "Show more",
   collapseButtonText = "Show less",
-  buttonColor,
+  buttonColor = "#1f09cd",
   expanded = false,
   className,
   children,
@@ -46,14 +46,20 @@ function TextExpander({
   const [isExpanded, setIsExpanded] = useState(expanded);
   const displayText = isExpanded
     ? children
-    : children
-        .split(" ", collapsedNumWords)
-        .slice(0, collapsedNumWords)
-        .join(" ") + "...";
+    : children.split(" ").slice(0, collapsedNumWords).join(" ") + "...";
+
+  const buttonStyle = {
+    background: "none",
+    border: "none",
+    font: "inherit",
+    cursor: "pointer",
+    marginLeft: "6px",
+    color: buttonColor,
+  };
   return (
     <div className={className}>
       <span>{displayText}</span>
-      <button onClick={() => setIsExpanded((exp) => !exp)}>
+      <button style={buttonStyle} onClick={() => setIsExpanded((exp) => !exp)}>
         {isExpanded ? collapseButtonText : expandButtonText}
       </button>
     </div>
